ee20852c05b1d2378ff7622ed8ad7b483a77dda1
SwastikNayak13 <swaxtik13@gmail.com>
2025-12-02 22:06:08 +0530

    day2 part2

---
+++ 2025/Day2/Part2.py
@@ -0,0 +1,27 @@
+labels = []
+
+with open("input.txt", "r") as f:
+    line = f.read().strip()
+    ranges = line.split(",")
+
+    for nums in ranges:
+        start, end = nums.split("-")
+        labels.append([int(start), int(end)])
+
+res = 0
+for n, m in labels:
+    for i in range(n, m + 1):
+        currStr = str(i)
+        for k in range(1, len(currStr) // 2 + 1):
+            p = currStr[:k]
+            flag = 1
+            for j in range(0 ,len(currStr), k):
+                q = currStr[j:j+k]
+                if p != q:
+                    flag = 0
+                    break
+            if flag:
+                res += i
+                break
+print(res)
+
